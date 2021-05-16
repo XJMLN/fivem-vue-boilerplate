df57a90f05f9b658010af4dee083033441bc5bc2
XJMLN <xjmln.programming@gmail.com>
2021-05-16 16:37:25 +0200

    1.0.0

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+    <div id="app">
+    </div>
+</template>
+
+
+<script>
+
+import Nui from './utils/Nui';
+
+export default {
+    name: "app",
+    components: {
+        //component
+    },
+    data() {
+        return {};
+    },
+    destroyed() {
+        window.removeEventListener("message", this.listener);
+    },
+    mouted() {
+        this.listener = window.addEventListener(
+          'message',
+          event => {
+              const element = event.data || event.detail;
+              if (this[element.type]) {
+                  this[element.type](element);
+              }
+          },
+          false,
+        );
+    }
+}
+</script>
+
+<style lang="scss">
+</style>
---
+++ src/components/component.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'component',
+  props: {
+  },
+};
+</script>
+<style scoped></style>
